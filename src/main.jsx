d0c36d6af2d6202076cffdfc8be7a58b23f84b16
yuriTakamisawaRibeiro <yuri.takamisawa.ribeiro@gmail.com>
2024-03-21 11:06:07 -0300

    update contact page

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';   
 import GlobalStyle from './styles/global';
-
-import { PetRegister } from './pages/PetRegister';
 import { Contact } from './pages/Contact'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
