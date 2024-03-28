e144a960857d18f6bc4d9a3f2272ed8c2ef26977
Bruno Bertholini <brbertholini@gmail.com>
2024-03-28 15:11:21 -0300

    feat: PetRegisterHeader steps working with StepButtons

--- src/components/StepsButtons/index.jsx
+++ src/components/StepsButtons/index.jsx
@@ -1,10 +1,16 @@
 import { Container, BackButton, NextButton } from "./styles";
+import PropTypes from 'prop-types';
 
-export function StepsButtons() {
+export function StepsButtons({ onNext, onBack }) {
     return(
         <Container>
-            <BackButton>Voltar</BackButton>
-            <NextButton>Continuar</NextButton>
+            <BackButton onClick={onBack}>Voltar</BackButton>
+            <NextButton onClick={onNext}>Continuar</NextButton>
         </Container>
     )
 }
+
+StepsButtons.propTypes = {
+    onNext: PropTypes.func.isRequired,
+    onBack: PropTypes.func.isRequired,
+};
--- src/pages/PetRegister/index.jsx
+++ src/pages/PetRegister/index.jsx
@@ -1,81 +1,94 @@
-import { Container, InfoIcon, Text, Form, Row, RowFile, InputTitle, InputPetRegister, DescriptionPet, InfoPet, LastInput } from "./styles"
+import { Container, InfoIcon, Text, Form, Row, RowFile, InputTitle, InputPetRegister, DescriptionPet, InfoPet, LastInput, TextAreaPetDescription } from "./styles"
 import { Select } from "../../components/Select"
 import { FileInput } from "../../components/FileInput"
 import { StepsButtons } from "../../components/StepsButtons"
 import { PetRegisterHeader } from "../../components/PetRegisterHeader"
-
-import { TextAreaPetDescription } from "./styles"
+import { useState } from "react"
 
 
 export function PetRegister() {
+    const [currentStep, setCurrentStep] = useState(0);
+
+    const handleNext = () => {
+        setCurrentStep(prevStep => prevStep + 1);
+    };
+
+    const handleBack = () => {
+        setCurrentStep(prevStep => prevStep - 1);
+    };
+
     return (
         <Container>
-            <PetRegisterHeader />
-            <InfoPet>
-            <h1>Registro de informações</h1>
-                <Text>
-                    <InfoIcon />
-                    <p>Apenas os campos com &quot;*&quot; são obrigatórios,
-                        caso você não saiba alguma informação dos demais campos,
-                        é permitido deixar em branco.
-                    </p>
-                </Text>
-                <Form>
-                    <Row>
-                        <div>
-                            <InputTitle>Espécie *</InputTitle>
-                            <InputPetRegister />
-                        </div>
-                        <div>
-                            <InputTitle>Sexo *</InputTitle>
-                            <Select />
-                        </div>
-                        <div>
-                            <InputTitle>Raça *</InputTitle>
-                            <InputPetRegister />
-                        </div>
-                    </Row>
-                    <Row>
-                        <div>
-                            <InputTitle>Temperamento</InputTitle>
-                            <InputPetRegister />
-                        </div>
-                        <div>
-                            <InputTitle>Porte *</InputTitle>
-                            <Select />
-                        </div>
-                        <div>
-                            <InputTitle>Cor *</InputTitle>
-                            <Select />
-                        </div>
-                    </Row>
-                    <RowFile>
-                        <div>
-                            <InputTitle>Fotos (min 2, max 6) *</InputTitle>
-                            <FileInput />
-                        </div>
-                    </RowFile>
-                </Form>
-            </InfoPet>
-            <DescriptionPet>
-                <h1>Descrição do animal</h1>
-                <Text>
-                    <InfoIcon />
-                    <p>Apenas os campos com &quot;*&quot; são obrigatórios,
-                        caso você não saiba alguma informação dos demais campos,
-                        é permitido deixar em branco.
-                    </p>
-                </Text>
-                <Form>
-                    <InputTitle>História do Pet *</InputTitle>
-                    <TextAreaPetDescription placeholder="Neste campo, conte a história que o levou até esse momento de colocar o pet para adoção" />
-                    <LastInput>
-                        <InputTitle>Cuidados Especiais *</InputTitle>
-                        <TextAreaPetDescription placeholder="Neste campo, informe qualquer tipo de necessidades especiais do pet" />
-                    </LastInput>
-                </Form>
-            </DescriptionPet>
-            <StepsButtons />
+            <PetRegisterHeader currentStep={currentStep} />
+            {currentStep === 0 && (
+                <InfoPet>
+                    <h1>Registro de informações</h1>
+                    <Text>
+                        <InfoIcon />
+                        <p>Apenas os campos com &quot;*&quot; são obrigatórios,
+                            caso você não saiba alguma informação dos demais campos,
+                            é permitido deixar em branco.
+                        </p>
+                    </Text>
+                    <Form>
+                        <Row>
+                            <div>
+                                <InputTitle>Espécie *</InputTitle>
+                                <InputPetRegister />
+                            </div>
+                            <div>
+                                <InputTitle>Sexo *</InputTitle>
+                                <Select />
+                            </div>
+                            <div>
+                                <InputTitle>Raça *</InputTitle>
+                                <InputPetRegister />
+                            </div>
+                        </Row>
+                        <Row>
+                            <div>
+                                <InputTitle>Temperamento</InputTitle>
+                                <InputPetRegister />
+                            </div>
+                            <div>
+                                <InputTitle>Porte *</InputTitle>
+                                <Select />
+                            </div>
+                            <div>
+                                <InputTitle>Cor *</InputTitle>
+                                <Select />
+                            </div>
+                        </Row>
+                        <RowFile>
+                            <div>
+                                <InputTitle>Fotos (min 2, max 6) *</InputTitle>
+                                <FileInput />
+                            </div>
+                        </RowFile>
+                    </Form>
+                </InfoPet>
+            )}
+            {currentStep === 1 && (
+                <DescriptionPet>
+                    <h1>Descrição do animal</h1>
+                    <Text>
+                        <InfoIcon />
+                        <p>Apenas os campos com &quot;*&quot; são obrigatórios,
+                            caso você não saiba alguma informação dos demais campos,
+                            é permitido deixar em branco.
+                        </p>
+                    </Text>
+                    <Form>
+                        <InputTitle>História do Pet *</InputTitle>
+                        <TextAreaPetDescription placeholder="Neste campo, conte a história que o levou até esse momento de colocar o pet para adoção" />
+                        <LastInput>
+                            <InputTitle>Cuidados Especiais *</InputTitle>
+                            <TextAreaPetDescription placeholder="Neste campo, informe qualquer tipo de necessidades especiais do pet" />
+                        </LastInput>
+                    </Form>
+                </DescriptionPet>
+            )}
+            <StepsButtons onNext={handleNext} onBack={handleBack} />
         </Container>
     )
 }
