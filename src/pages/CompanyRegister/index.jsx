807ea45f6104c71c98b8fcd493d02938717c1ad9
Bruno Bertholini <brbertholini@gmail.com>
2024-04-02 10:55:30 -0300

    fix: Updating SideInfo styles

--- src/pages/CompanyRegister/index.jsx
+++ src/pages/CompanyRegister/index.jsx
@@ -1,5 +1,6 @@
 import AdopetImg from '../../assets/images/AdopetLogo.svg';
-import { Container, SideInfo, Title } from './styles';
+import { Row } from '../PetRegister/styles';
+import { Container, Info, SideInfo, Title, Form, RegisterContent } from './styles';
 
 export function CompanyRegister() {
     return(
@@ -10,6 +11,15 @@ export function CompanyRegister() {
                 <p>Na Adopet, acreditamos que o verdadeiro impacto é feito através da colaboração e do trabalho em equipe. É por isso que estamos sempre em busca de parcerias com empresas que compartilham nossa paixão por ajudar animais em busca de um lar amoroso.
 Ao se tornar um parceiro da Adopet, sua empresa terá a oportunidade única de fazer uma diferença significativa na vida de animais em situação de vulnerabilidade. Sua contribuição não apenas ajuda a salvar vidas, mas também demonstra o compromisso da sua empresa com a responsabilidade social e o bem-estar animal.</p>
             </SideInfo>
+            <RegisterContent>
+            <Info>
+                <h1>Enviar Registro</h1>
+                <p>Preencha o formulário e solicite o registro da sua empresa</p>
+            </Info>
+            <Form>
+                <Row></Row>
+            </Form>
+            </RegisterContent>
         </Container>
     )
 }
--- src/pages/CompanyRegister/styles.js
+++ src/pages/CompanyRegister/styles.js
@@ -2,18 +2,22 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     width: 100%;
+    display: flex;
+    flex-direction: row;
 `
 
 export const SideInfo = styled.div`
     padding-top: 40px;
     background-color: #E8E8E0;
-    gap: 30px;
+    gap: 20px;
     width: 296px;
+    height: 100vh;
     display: flex;
     flex-direction: column;
     align-items: center;
     text-align: center;
     > p {
+        font-size: 12px;
         text-align: justify;
         margin-left: 20px;
         margin-right: 20px;
@@ -21,8 +25,27 @@ export const SideInfo = styled.div`
 `
 
 export const Title = styled.h1`
-    font-size: 24px;
+    font-size: 20px;
     font-weight: bold;
     width: 80%;
     color: #E4AC46;
+`
+
+export const Info = styled.div`
+    padding-top: 8vh;
+    padding-bottom: 8vh;
+    width: 70%;
+    text-align: center;
+`
+
+export const Form = styled.form`
+    width: 100%;
+
+`
+
+export const RegisterContent = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 `
