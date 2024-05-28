7b85a1c9477066d4bb78971eebe31a17ec2935a2
brbertholini <brbertholini@gmail.com>
2024-05-28 10:57:22 -0300

    add post doc register

--- src/pages/PostRegister/index.jsx
+++ src/pages/PostRegister/index.jsx
@@ -1,9 +1,12 @@
 import { FileInput } from "../../components/FileInput";
 import { Select } from "../../components/Select";
 import { Container, Form, InfoSection, InputPostAuthor, InputPostContent, InputPostDescription, InputPostTitle, InputTitle, PostButton, Row } from "./styles";
+import { firestore, storage } from "../../services/firebaseConfig";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { ref, uploadBytes } from "firebase/storage";
+import { useState } from "react";
 
 export function PostRegister() {
-
     const optionsCategories = [
         { label: 'Selecione uma opção', value: '' },
         { label: 'Treinamento', value: 'trainment' },
@@ -11,6 +14,67 @@ export function PostRegister() {
         { label: 'Diversão', value: 'fun' },
         { label: 'Cuidados', value: 'care' },
     ];
+
+    const [formData, setFormData] = useState({
+        title: "",
+        category: "",
+        description: "",
+        content: "",
+        author: "",
+        created_at: serverTimestamp(),
+        files: [],
+    });
+
+    const [files, setFiles] = useState([]);
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
+
+    const [selectedOption, setSelectedOption] = useState('');
+
+    const handleCategoryChange = (newValue) => {
+      setSelectedOption(newValue);
+    };
+
+    const handleFileChange = (selectedFiles) => {
+        // Convert FileList to an array
+        const filesArray = Array.from(selectedFiles);
+        // Update the state with the selected files
+        setFiles(filesArray);
+    };
+
+    const handleSubmit = async () => {
+        try {
+            // Adiciona os dados do formData ao Firestore
+            const posts = collection(firestore, "posts");
+            const docRef = await addDoc(posts, formData);
+            console.log("Documento adicionado com ID:", docRef.id);
+
+            // Envia os arquivos para o Storage
+            const files = formData.selectedFiles; // Obtém a lista de arquivos selecionados
+            const postFileRef = ref(storage, `posts/${docRef.id}`);
+
+            // Itera sobre cada arquivo e envia para o Storage
+            files.forEach(async (file) => {
+                const fileRef = ref(postFileRef, file.name);
+                await uploadBytes(fileRef, file);
+                console.log(`Arquivo ${file.name} enviado com sucesso.`);
+            });
+
+            Object.keys(files).forEach((fileKey) => {
+                const file = files[fileKey];
+                const storageRef = ref(storage, `posts/${docRef.id}/${file.name}`);
+                const uploadTask = uploadBytes(storageRef, file);
+            });
+        } catch (error) {
+            console.error("Erro ao adicionar documento:", error);
+        }
+    };
 
     return (
         <Container>
@@ -22,38 +86,62 @@ export function PostRegister() {
                 <Row>
                     <div>
                         <InputTitle>Título da postagem</InputTitle>
-                        <InputPostTitle />
+                        <InputPostTitle
+                            name="title"
+                            value={formData.title}
+                            onChange={handleInputChange}
+                        />
                     </div>
                     <div className="post-category">
                         <InputTitle>Categoria da postagem</InputTitle>
-                        <Select options={optionsCategories} />
+                        <Select
+                            options={optionsCategories}
+                            name="category"
+                            value={formData.category}
+                            onChange={handleCategoryChange}
+                        />
                     </div>
                 </Row>
                 <Row>
                     <div className="post-description">
                         <InputTitle>Descrição da postagem</InputTitle>
-                        <InputPostDescription />
+                        <InputPostDescription
+                            name="description"
+                            value={formData.description}
+                            onChange={handleInputChange}
+                        />
                     </div>
                 </Row>
                 <Row>
                     <div className="post-content">
                         <InputTitle>Conteúdo da postagem</InputTitle>
-                        <InputPostContent />
+                        <InputPostContent
+                            name="content"
+                            value={formData.content}
+                            onChange={handleInputChange}
+                        />
                     </div>
                 </Row>
                 <Row>
                     <div className="post-image">
                         <InputTitle>Imagem de capa da postagem</InputTitle>
-                        <FileInput />
+                        <FileInput onChange={handleFileChange} />
                     </div>
                 </Row>
                 <Row>
                     <div>
                         <InputTitle>Autor da postagem</InputTitle>
-                        <InputPostAuthor />
+                        <InputPostAuthor
+                            name="author"
+                            value={formData.author}
+                            onChange={handleInputChange}
+                        />
                     </div>
                     <div>
-                        <PostButton>Fazer postagem</PostButton>
+                        <PostButton
+                            type="button"
+                            onClick={handleSubmit}
+                        >Fazer postagem</PostButton>
                     </div>
                 </Row>
             </Form>
