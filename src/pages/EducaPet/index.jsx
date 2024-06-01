ef289dd7b25b3c8b395196ef081e1f7ded901d52
Bruno Bertholini <brbertholini@gmail.com>
2024-06-01 15:57:26 -0300

    get posts

--- src/pages/EducaPet/index.jsx
+++ src/pages/EducaPet/index.jsx
@@ -3,11 +3,33 @@ import { Input } from "../../components/Input";
 import { Commands, Container, Content, FilterDate, FilterDiv, Filters, ListDiv, MotherNew, New, SearchDiv, Topics } from "./styles";
 import { BiSearchAlt } from "react-icons/bi";
 import { GrCluster } from "react-icons/gr";
-import imageExample from "../../assets/images/educapetExample.png"
+import imageExample from "../../assets/images/educapetExample.png";
+import { collection, getDocs } from "firebase/firestore"; // Updated import
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import { useEffect, useState } from "react";
+import Post from "../../components/Post";
+import { db } from "../../services/firebaseConfig";
 
 export function EducaPet() {
-    
+    const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const querySnapshot = await getDocs(collection(db, 'posts')); // Use db and specify the collection path
+                const fetchedPosts = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+                setPosts(fetchedPosts);
+            } catch (error) {
+                console.error('Error fetching documents:', error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, []);
+
     return (
         <Container>
             <Header />
@@ -21,8 +43,6 @@ export function EducaPet() {
             </SearchDiv>
 
             <Content>
-
-
                 <FilterDiv>
                     <MotherNew>
                         <img src={imageExample} alt="" />
@@ -32,9 +52,9 @@ export function EducaPet() {
                             <p> 28 Mar, 2024</p>
                         </div>
                         <h1>Guiando o Caminho: Como Educar um Filhote de Cachorro - 6 Pontos Fundamentais</h1>
-                        <p>Educar um filhote de cachorro pode ser uma jornada recompensadora, mas requer paciência, consistência e conhecimento. 
-                            Neste artigo, exploramos os seis pontos essenciais para criar um filhote feliz, saudável e bem-comportado. 
-                            Desde estabelecer uma rotina sólida até socialização adequada e treinamento básico, cada ponto é crucial 
+                        <p>Educar um filhote de cachorro pode ser uma jornada recompensadora, mas requer paciência, consistência e conhecimento.
+                            Neste artigo, exploramos os seis pontos essenciais para criar um filhote feliz, saudável e bem-comportado.
+                            Desde estabelecer uma rotina sólida até socialização adequada e treinamento básico, cada ponto é crucial
                             para o desenvolvimento positivo do seu novo companheiro canino.</p>
 
                         <div>
@@ -51,9 +71,9 @@ export function EducaPet() {
                                 <li>Nóticias</li>
                             </ul>
                         </Topics>
-                
+
                         <FilterDate>
-                        
+
                             <label htmlFor="selectDate">Filtrar por</label>
                             <select id="selectDate">
                                 <option value="latest">Mais Recente</option>
@@ -61,117 +81,31 @@ export function EducaPet() {
                             </select>
                         </FilterDate>
                     </Filters>
-            
+
                 </FilterDiv>
                 <ListDiv>
-                    <New>
-                    <img src={imageExample} alt="" />
-                        <div>
-                            <GrCluster />
-                            <h5>Treinamento</h5>
-                            <p> 28 Mar, 2024</p>
-                        </div>
-                        <h1>Guiando o Caminho: Como Educar um Filhote de Cachorro - 6 Pontos Fundamentais</h1>
-                        <p>Educar um filhote de cachorro pode ser uma jornada recompensadora, mas requer paciência, consistência e conhecimento. 
-                            Neste artigo, exploramos os seis pontos essenciais para criar um filhote feliz, saudável e bem-comportado. 
-                            Desde estabelecer uma rotina sólida até socialização adequada e treinamento básico, cada ponto é crucial 
-                            para o desenvolvimento positivo do seu novo companheiro canino.</p>
-
-                        <div>
-                            <p>Autor | Gabriel Maia</p>
-                        </div>
-                    </New>
-                    <New>
-                    <img src={imageExample} alt="" />
-                        <div>
-                            <GrCluster />
-                            <h5>Treinamento</h5>
-                            <p> 28 Mar, 2024</p>
-                        </div>
-                        <h1>Guiando o Caminho: Como Educar um Filhote de Cachorro - 6 Pontos Fundamentais</h1>
-                        <p>Educar um filhote de cachorro pode ser uma jornada recompensadora, mas requer paciência, consistência e conhecimento. 
-                            Neste artigo, exploramos os seis pontos essenciais para criar um filhote feliz, saudável e bem-comportado. 
-                            Desde estabelecer uma rotina sólida até socialização adequada e treinamento básico, cada ponto é crucial 
-                            para o desenvolvimento positivo do seu novo companheiro canino.</p>
-
-                        <div>
-                            <p>Autor | Gabriel Maia</p>
-                        </div>
-                    </New>
-                    <New>
-                    <img src={imageExample} alt="" />
-                        <div>
-                            <GrCluster />
-                            <h5>Treinamento</h5>
-                            <p> 28 Mar, 2024</p>
-                        </div>
-                        <h1>Guiando o Caminho: Como Educar um Filhote de Cachorro - 6 Pontos Fundamentais</h1>
-                        <p>Educar um filhote de cachorro pode ser uma jornada recompensadora, mas requer paciência, consistência e conhecimento. 
-                            Neste artigo, exploramos os seis pontos essenciais para criar um filhote feliz, saudável e bem-comportado. 
-                            Desde estabelecer uma rotina sólida até socialização adequada e treinamento básico, cada ponto é crucial 
-                            para o desenvolvimento positivo do seu novo companheiro canino.</p>
-
-                        <div>
-                            <p>Autor | Gabriel Maia</p>
-                        </div>
-                    </New>
-                    <New>
-                    <img src={imageExample} alt="" />
-                        <div>
-                            <GrCluster />
-                            <h5>Treinamento</h5>
-                            <p> 28 Mar, 2024</p>
-                        </div>
-                        <h1>Guiando o Caminho: Como Educar um Filhote de Cachorro - 6 Pontos Fundamentais</h1>
-                        <p>Educar um filhote de cachorro pode ser uma jornada recompensadora, mas requer paciência, consistência e conhecimento. 
-                            Neste artigo, exploramos os seis pontos essenciais para criar um filhote feliz, saudável e bem-comportado. 
-                            Desde estabelecer uma rotina sólida até socialização adequada e treinamento básico, cada ponto é crucial 
-                            para o desenvolvimento positivo do seu novo companheiro canino.</p>
-
-                        <div>
-                            <p>Autor | Gabriel Maia</p>
-                        </div>
-                    </New>
-                    <New>
-                    <img src={imageExample} alt="" />
-                        <div>
-                            <GrCluster />
-                            <h5>Treinamento</h5>
-                            <p> 28 Mar, 2024</p>
-                        </div>
-                        <h1>Guiando o Caminho: Como Educar um Filhote de Cachorro - 6 Pontos Fundamentais</h1>
-                        <p>Educar um filhote de cachorro pode ser uma jornada recompensadora, mas requer paciência, consistência e conhecimento. 
-                            Neste artigo, exploramos os seis pontos essenciais para criar um filhote feliz, saudável e bem-comportado. 
-                            Desde estabelecer uma rotina sólida até socialização adequada e treinamento básico, cada ponto é crucial 
-                            para o desenvolvimento positivo do seu novo companheiro canino.</p>
-
-                        <div>
-                            <p>Autor | Gabriel Maia</p>
-                        </div>
-                    </New>
-                    <New>
-                    <img src={imageExample} alt="" />
-                        <div>
-                            <GrCluster />
-                            <h5>Treinamento</h5>
-                            <p> 28 Mar, 2024</p>
-                        </div>
-                        <h1>Guiando o Caminho: Como Educar um Filhote de Cachorro - 6 Pontos Fundamentais</h1>
-                        <p>Educar um filhote de cachorro pode ser uma jornada recompensadora, mas requer paciência, consistência e conhecimento. 
-                            Neste artigo, exploramos os seis pontos essenciais para criar um filhote feliz, saudável e bem-comportado. 
-                            Desde estabelecer uma rotina sólida até socialização adequada e treinamento básico, cada ponto é crucial 
-                            para o desenvolvimento positivo do seu novo companheiro canino.</p>
-
-                        <div>
-                            <p>Autor | Gabriel Maia</p>
-                        </div>
-                    </New>
+                    {loading ? (
+                        <p>Loading...</p>
+                    ) : (
+                        posts.map(post => (
+                            <Post
+                                key={post.id}
+                                title={post.title}
+                                date={post.date}
+                                description={post.description}
+                                content={post.content}
+                                author={post.author}
+                                category={post.category}
+                                image_url={post.image_url}
+                            />
+                        ))
+                    )}
                 </ListDiv>
                 <Commands>
-                    <button><IoIosArrowBack/></button>
+                    <button><IoIosArrowBack /></button>
                     <button> 1 </button>
                     <button> 2 </button>
-                    <button><IoIosArrowForward/></button>
+                    <button><IoIosArrowForward /></button>
 
                 </Commands>
             </Content>
--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -18,6 +18,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
 export const auth = getAuth(app)
 export const firestore = getFirestore(app)
 export const storage = getStorage(app)
