0815c8653c09a14f06c9393576955ea164663221
Bruno Bertholini <brbertholini@gmail.com>
2024-06-01 16:25:09 -0300

    category translations

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -2,16 +2,23 @@ import React from 'react';
 import { GrCluster } from "react-icons/gr";
 import { Container } from './styles';
 
+const categoryTranslations = {
+    'trainment': 'Treinamento',
+    'pet-health': 'Saúde',
+    'fun': 'Diversão',
+    'care': 'Cuidados',
+};
+
 const Post = ({ title, date, description, author, category, image_url }) => {
 
-    console.log(image_url)
+    const translatedCategory = categoryTranslations[category] || category;
 
     return (
         <Container>
             <img src={image_url} alt="" />
             <div>
                 <GrCluster />
-                <h5>{category}</h5>
+                <h5>{translatedCategory}</h5>
                 <p>{date}</p>
             </div>
             <h1>{title}</h1>
